8588a16e80491d7551243a7bde6c2e438d894829
alinen <alinen@savvysine.com>
2022-11-23 17:29:50 -0500

    post asst10

--- Recursion.java
+++ Recursion.java
@@ -1,22 +1,23 @@
 import java.util.Arrays;
 
-class Recursion {
+class RecursionSoln {
+    
+    public static void rblastoff(int val) {
+        return;
+    }
+
+    public static int rgetMod() {
+        return 1;
+    }
 
     public static int reven(int n) {
         return 0;
     }
-    
+
     public static String rrevStr(String text) {
         return "";
     }
-    
-    public static void rblastoff(int val) {
-    }
-    
-    public static int rgetMod() {
-        return 0;
-    }
-    
+
     public static String rxerox(String word, int n) {
         return "";
     }
@@ -26,10 +27,6 @@ class Recursion {
     }
 
     public static boolean risPalindrome(String text) {
-        return false; 
-    }
-    
-    public static boolean rinString(char x, String word) {
         return false;
     }
     
@@ -37,56 +34,105 @@ class Recursion {
         return 0;
     }
 
-    public static void test(Object o1, Object o2) throws Exception {
-        if (o1 != o2) {
-            throw new Exception("Test Failed: "+o1+" != "+o2);
-        }
+    public static int rmax(int[] lst) {
+        return 0;
     }
 
-    public static void test(String o1, String o2) throws Exception {
-        if (o1.compareTo(o2) != 0) {
-            throw new Exception("Test Failed: "+o1+" != "+o2);
+    public static double raverage(int[] L) {
+    }
+
+    public static void rreverse(int[] L) {
+    }
+
+    public static void test(String msg, Object o1, Object o2) throws Exception {
+        if (o1 != o2) {
+            throw new Exception("Failed: "+msg+" "+o1+" != "+o2);
         }
+        System.out.println("SUCCESS: "+msg+" "+o1+" = "+o2);
+    }
+
+    public static void test(String msg, double o1, double o2) throws Exception {
+        if (Math.abs(o1 - o2) > 0.00001) {
+            throw new Exception("Failed: "+msg+" "+o1+" != "+o2);
+        }
+        System.out.println("SUCCESS: "+msg+" "+o1+" = "+o2);
+    }
+
+    public static void test(String msg, String o1, String o2) throws Exception {
+        if (o1.compareTo(o2) != 0) {
+            throw new Exception("Failed: "+msg+" "+o1+" != "+o2);
+        }
+        System.out.println("SUCCESS: "+msg+" "+o1+" = "+o2);
+    }
+
+    public static void test(String msg, int[] o1, int[] o2) throws Exception {
+        if (o1.length != o2.length) {
+            throw new Exception("Failed (length): "+msg+" "
+               +Arrays.toString(o1) + " != "+Arrays.toString(o2));
+        }
+
+        for (int i = 0; i < o1.length; i++) {
+            if (o1[i] != o2[i]) {
+                throw new Exception("Failed (value): "+msg+" "
+                    +Arrays.toString(o1) + " != "+Arrays.toString(o2));
+            }
+        }
+        System.out.println("SUCCESS: "+msg+ " "
+            +Arrays.toString(o1) + " = "+Arrays.toString(o2));
     }
     
     public static void main(String[] args) {
         
         try {
             rblastoff(5);
+            test ("rgetMod", rgetMod() % 3, 0);
 
-            test (reven(10), 30); 
-            test (reven(1), 0); 
+            test ("reven", reven(10), 30); 
+            test ("reven", reven(11), 30); 
+            test ("reven", reven(1), 0); 
 
-            test (rrevStr("hello"), "olleh"); 
-            test (rrevStr("A"), "A"); 
-            test (rrevStr(""), "");
-            test (rrevStr("HANNAH"), "HANNAH");
+            test ("rrevStr", rrevStr("hello"), "olleh"); 
+            test ("rrevStr", rrevStr("A"), "A"); 
+            test ("rrevStr", rrevStr(""), "");
+            test ("rrevStr", rrevStr("HANNAH"), "HANNAH");
             
-            test (rxerox("la", 4), "lalalala");
-            test (rxerox("la", 0), "");
+            test ("rxerox", rxerox("la", 4), "lalalala");
+            test ("rxerox", rxerox("la", 0), "");
 
-            test (rraise(2,3) ,  8);
-            test (rraise(2,0) ,  1);
+            test ("rraise", rraise(2,3) ,  8);
+            test ("rraise", rraise(2,0) ,  1);
 
-            test (risPalindrome("abba") ,  true);
-            test (risPalindrome("apple") ,  false);
-            test (risPalindrome("8") ,  true);
-            test (risPalindrome("$-$") ,  true);
-            test (risPalindrome("") ,  true);
+            test ("risPalindrome", risPalindrome("abba") ,  true);
+            test ("risPalindrome", risPalindrome("apple") ,  false);
+            test ("risPalindrome", risPalindrome("8") ,  true);
+            test ("risPalindrome", risPalindrome("$-$") ,  true);
+            test ("risPalindrome", risPalindrome("") ,  true);
 
-            test (rinString('0', "4,6,0") ,  true);
-            test (rinString('0', "4,6,4") ,  false);
-            test (rinString('0', "") ,  false);
+            test ("rcount", rcount('a', "abba") ,  2);
+            test ("rcount", rcount('a', "obbi") , 0);
 
-            test (rgetMod() % 3, 0);
+            test ("raverage", raverage(new int[]{2, 5, 10, 8}), 25/4.0);
+            test ("raverage", raverage(new int[]{}), 0);
 
-            test (rcount('a', "abba") ,  2);
-            test (rcount('a', "obbi") , 0);
+            test ("rmax", rmax(new int[]{2, 5, 10, 8}), 10);
+            test ("rmax", rmax(new int[]{10, 5, 6, 8}), 10);
+            test ("rmax", rmax(new int[]{2}), 2);
+            test ("rmax", rmax(new int[]{}), -1);
+
+            int[] L1 = {2, 5, 10, 8};
+            int[] L3 = {2, 5, 8};
+            int[] L2 = {};
+            rreverse(L1);
+            rreverse(L2);
+            rreverse(L3);
+
+            test ("rreverse", L1, new int[]{8, 10, 5, 2});
+            test ("rreverse", L3, new int[]{8, 5, 2});
+            test ("rreverse", L2, new int[]{});
         }
         catch (Exception e)
         {
             e.printStackTrace();
         }
     }
-    
 }
